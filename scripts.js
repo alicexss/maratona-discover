eec1a7495cd8048e18dd261742758d2dc720bfec
alicexss <alicexaviers21@outlook.com>
2021-06-15 16:02:29 -0300

    fixing the error and ending

--- scripts.js
+++ scripts.js
@@ -107,10 +107,10 @@ const DOM = {
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value) * 100
+    value = value * 100
 
-    return value
 
+    return Math.round(value)
   },
 
   formatDate(date) {
